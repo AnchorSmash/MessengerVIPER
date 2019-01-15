2024741bb962f358fd7c853aca3af35f0ca196a7
Viktor <vicodin1080@gmail.com>
2019-01-15 14:13:24 +0300

    Initial Commit

---
+++ MessengerVIPER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessengerVIPER
+//
+//  Created by ShadowMoon on 15/01/2019.
+//  Copyright © 2019 Виктор Кулаков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
